2fcf48c742fc8ec9901e5d93ed31406cfca45669
lyfenwebos <mrexoduso@gmail.com>
2017-10-28 22:09:13 +0300

    0.1.5

--- QuestionnaireApp/MainForm.cs
+++ QuestionnaireApp/MainForm.cs
@@ -56,9 +56,9 @@ namespace QuestionannaireApp
         public int[] versionCheckArray = new int[3];
 
 
-        public string currentVersion = "0.1.4";
-        public int[] currentVersionArray = { 0, 1, 4 };
-        public string[] previousVersions = { "0.0.1", "0.0.2", "0.0.3", "0.1.1","0.1.2","0.1.3"};
+        public string currentVersion = "0.1.3";
+        public int[] currentVersionArray = { 0, 1, 3 };
+        public string[] previousVersions = { "QuestionannaireApp.exe","0.0.1", "0.0.2", "0.0.3", "0.1.1","0.1.2"};
         
         public MainForm()
         {
