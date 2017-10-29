12a6d380c3bcdfb0b0572623fcd216f0458e3d28
lyfenwebos <mrexoduso@gmail.com>
2017-10-29 03:01:33 +0300

    Seems like a bug, since i miswrote app version, updating Version info.

--- QuestionnaireApp/MainForm.cs
+++ QuestionnaireApp/MainForm.cs
@@ -61,9 +61,9 @@ namespace QuestionannaireApp
         public int[] versionCheckArray = new int[3];
 
 
-        public string currentVersion = "0.2.1";
-        public int[] currentVersionArray = { 0, 2, 1 };
-        public string[] previousVersions = { "0.0.1", "0.0.2", "0.0.3", "0.1.1","0.1.2","0.1.3","0.1.4","0.1.5", "0.1.6", "0.2.0"};
+        public string currentVersion = "0.2.3";
+        public int[] currentVersionArray = { 0, 2, 3 };
+        public string[] previousVersions = { "0.0.1", "0.0.2", "0.0.3", "0.1.1","0.1.2","0.1.3","0.1.4","0.1.5", "0.1.6", "0.2.0","0.2.1"};
         
         public MainForm()
         {
