7264370364c98705a6805f9f99f5cfb392a34de8
lyfenwebos <mrexoduso@gmail.com>
2018-03-30 13:26:44 +0300

    About window

--- QuestionnaireApp/About.Designer.cs
+++ QuestionnaireApp/About.Designer.cs
@@ -28,19 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.label1 = new System.Windows.Forms.Label();
+            this.aboutTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-8, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // aboutTextBox
+            // 
+            this.aboutTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.aboutTextBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutTextBox.Location = new System.Drawing.Point(0, 0);
+            this.aboutTextBox.Multiline = true;
+            this.aboutTextBox.Name = "aboutTextBox";
+            this.aboutTextBox.ReadOnly = true;
+            this.aboutTextBox.Size = new System.Drawing.Size(453, 268);
+            this.aboutTextBox.TabIndex = 2;
+            this.aboutTextBox.TabStop = false;
             // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(453, 268);
+            this.Controls.Add(this.aboutTextBox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "About";
             this.Text = "About";
+            this.Load += new System.EventHandler(this.About_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox aboutTextBox;
     }
 }
--- QuestionnaireApp/About.cs
+++ QuestionnaireApp/About.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.IsolatedStorage;
+using System.IO;
 
 namespace QuestionannaireApp
 {
@@ -16,5 +18,105 @@ namespace QuestionannaireApp
         {
             InitializeComponent();
         }
+
+        private void About_Load(object sender, EventArgs e)
+        {
+            string licenseKey=String.Empty;
+            if (Properties.Settings.Default.block == false)
+            {
+                using (IsolatedStorageFile isolatedStorageFile = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null))
+                {
+                    using (IsolatedStorageFileStream isolatedStorageFileStream = new IsolatedStorageFileStream("license.lic", System.IO.FileMode.Open, isolatedStorageFile))
+                    {
+                        using (System.IO.StreamReader sr = new System.IO.StreamReader(isolatedStorageFileStream))
+                        {
+                            licenseKey = sr.ReadLine();
+                        }
+                    }
+                }
+            }
+            if (Properties.Settings.Default.culture == "ru-RU")
+            {
+                if (Properties.Settings.Default.block == true)
+                {
+                    this.aboutTextBox.Text = "Программа представленна \"Как есть\". Разработчик не несет никакой ответственности.\r\n\r\n" +
+                        "По всем вопросам обращаться по почте admin@exordium.cloud\r\n\r\n" +
+                        "Разработчик Павел Гончаров." +
+                        "\r\nВерсия ПО " + ProductVersion + "\r\n\r\n" +
+                        "Тип лицензии - Бесплатная";
+                }
+                else
+                {
+                    this.aboutTextBox.Text = "Программа представленна \"Как есть\". Разработчик не несет никакой ответственности.\r\n\r\n" +
+                        "По всем вопросам обращаться по почте admin@exordium.cloud\r\n\r\n" +
+                        "Разработчик Павел Гончаров." +
+                        "\r\nВерсия ПО " + ProductVersion + "\r\n\r\n" +
+                        "Тип лицензии - Платная\r\n" +
+                        "Ключ - "+licenseKey;
+                }
+            }
+            else if (Properties.Settings.Default.culture == "et-EE")
+            {
+                if (Properties.Settings.Default.block == true)
+                {
+                    this.aboutTextBox.Text = "Programm on esitatud \"Mis on\". Arendaja ei vastuta.\r\n\r\n" +
+                        "Kõigi küsimuste korral võtke palun ühendust posti teel admin@exordium.cloud\r\n\r\n" +
+                        "Arendaja Pavel Gontsarov." +
+                        "\r\nTarkvara versioon " + ProductVersion + "\r\n\r\n" +
+                        "Litsentsi tüüp - Tasuta";
+                }
+                else
+                {
+                    this.aboutTextBox.Text = "Programm on esitatud \"Mis on\". Arendaja ei vastuta.\r\n\r\n" +
+                        "Kõigi küsimuste korral võtke palun ühendust posti teel admin@exordium.cloud\r\n\r\n" +
+                        "Arendaja Pavel Gontsarov." +
+                        "\r\nTarkvara versioon " + ProductVersion + "\r\n\r\n" +
+                        "Litsentsi tüüp - Tasuline\r\n" +
+                        "Litsentsivõti - "+licenseKey;
+                }
+            }
+            else if (Properties.Settings.Default.culture == "gb-EN")
+            {
+                if (Properties.Settings.Default.block == true)
+                {
+                    this.aboutTextBox.Text = "The program is presented \"AS IS\". The developer does not bear any responsibility.\r\n\r\n" +
+                        "For all questions please contact by mail admin@exordium.cloud\r\n\r\n" +
+                        "Developer Pavel Gontsarov." +
+                        "\r\nSoftware Version " + ProductVersion + "\r\n\r\n" +
+                        "License type - Free";
+                }
+                else
+                {
+                    this.aboutTextBox.Text = "The program is presented \"AS IS\". The developer does not bear any responsibility.\r\n\r\n" +
+                        "For all questions please contact by mail admin@exordium.cloud\r\n\r\n" +
+                        "Developer Pavel Gontsarov." +
+                        "\r\nSoftware Version " + ProductVersion + "\r\n\r\n" +
+                        "License type - Paid\r\n" +
+                        "License key - "+licenseKey;
+                }
+            }
+            else
+            {
+                if (Properties.Settings.Default.block == true)
+                {
+                    this.aboutTextBox.Text = "The program is presented \"AS IS\". The developer does not bear any responsibility.\r\n\r\n" +
+                        "For all questions please contact by mail admin@exordium.cloud\r\n\r\n" +
+                        "Developer Pavel Gontsarov." +
+                        "\r\nSoftware Version " + ProductVersion + "\r\n\r\n" +
+                        "License type - Free";
+                }
+                else
+                {
+                    this.aboutTextBox.Text = "The program is presented \"AS IS\". The developer does not bear any responsibility.\r\n\r\n" +
+                        "For all questions please contact by mail admin@exordium.cloud\r\n\r\n" +
+                        "Developer Pavel Gontsarov." +
+                        "\r\nSoftware Version " + ProductVersion + "\r\n\r\n" +
+                        "License type - Paid\r\n" +
+                        "License key - " + licenseKey;
+                }
+            }
+        }
     }
 }
+    
+
